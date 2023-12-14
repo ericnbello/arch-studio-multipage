53bae3a9edb7f36d1753bf6c97aae63081924cbe
Eric Bello <ebello219@gmail.com>
2023-12-14 13:43:11 -0500

    updated module  exports

--- next.config.js
+++ next.config.js
@@ -1,19 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  images: {
-    domains: ['maps.googleapis.com'],
-  },
-  outputStandalone: true,
-}
-
-module.exports = nextConfig
-
-// module.exports = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
 //   reactStrictMode: false,
 //   images: {
 //     domains: ['maps.googleapis.com'],
 //   },
-//   output: 'standalone',
+//   outputStandalone: true,
+  
 // }
+
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+    domains: ['maps.googleapis.com'],
+  },
+  output: 'standalone',
+}
 
