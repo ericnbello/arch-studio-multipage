84d6c9ae8a1c2eefdf6e8eaf22e4af2b2eb969c4
Eric Bello <ebello219@gmail.com>
2023-12-14 13:08:43 -0500

    updated image domain

--- next.config.js
+++ next.config.js
@@ -1,32 +1,19 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    domains: ['maps.googleapis.com'],
+  },
+  outputStandalone: true,
+}
+
+module.exports = nextConfig
+
+// module.exports = {
 //   reactStrictMode: false,
 //   images: {
 //     domains: ['maps.googleapis.com'],
 //   },
-//   outputStandalone: true,
+//   output: 'standalone',
 // }
 
-// module.exports = nextConfig
-
-// // module.exports = {
-// //   reactStrictMode: false,
-// //   images: {
-// //     domains: ['maps.googleapis.com'],
-// //   },
-// //   output: 'standalone',
-// // }
-
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: 'standalone',
-  exportPathMap: async function (defaultPathMap) {
-    return {
-      '/': { page: '/' },
-    };
-  },
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-}
